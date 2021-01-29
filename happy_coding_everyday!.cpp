3958770e508ffe98528f4415bbe55d84c857c80b
Epochex Lin <68621077+Epochex@users.noreply.github.com>
2021-01-29 20:56:05 +0800

    the first console
    just for experiment

---
+++ happy_coding_everyday!.cpp
@@ -0,0 +1,13 @@
+﻿#include<stdio.h>
+
+int main() {
+	typedef long int64_t;
+	typedef struct Adate {
+		int  day;
+		int month;
+		int year;
+	}date;
+	int64_t i = 114514;
+	date d = { 25,114,245 };
+	printf("%i %i %i ", d);
+}
